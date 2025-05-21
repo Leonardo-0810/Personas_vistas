e2ea17883692e20893ff57f243877211d9fe0ee2
unknown <jsotto2003@gmail.com>
2025-05-21 08:36:43 -0500

    correccion boton editar

--- src/components/Comuna/EditarComuna.vue
+++ src/components/Comuna/EditarComuna.vue
@@ -7,15 +7,16 @@
       </div>
 
       <div class="card-body">
-        <form @submit.prevent="updateComuna">
+        <!-- SOLO RENDERIZAR CUANDO comuna YA CARGÓ -->
+        <form v-if="comuna" @submit.prevent="updateComuna">
           <div class="row mb-3">
-            <label for="comu_cod" class="form-label">Código:</label>
+            <label for="comu_codi" class="form-label">Código:</label>
             <div class="input-group">
               <div class="input-group-text">
                 <font-awesome-icon icon="id" />
               </div>
-              <input type="text" class="form-control" id="comu_cod" placeholder="Código comuna" disabled
-                v-model="comuna.comu_cod">
+              <input type="text" class="form-control" id="comu_codi" placeholder="Código comuna" disabled
+                v-model="comuna.comu_codi">
             </div>
           </div>
 
@@ -25,25 +26,27 @@
               <div class="input-group-text">
                 <font-awesome-icon icon="building" />
               </div>
-              <input type="text" class="form-control" id="comu_nomb" placeholder="Código comuna"
+              <input type="text" class="form-control" id="comu_nomb" placeholder="Nombre comuna"
                 v-model="comuna.comu_nomb">
             </div>
           </div>
 
           <div class="row mb-3">
-            <label for="muni_cod" class="form-label">Municipio:</label>
+            <label for="muni_codi" class="form-label">Municipio:</label>
             <div class="input-group">
               <div class="input-group-text">
                 <font-awesome-icon icon="bank" />
               </div>
-              <select class="form-control" id="muni_cod" v-model="comuna.muni_cod">
-                <option v-for="municipio in municipios" :value="municipio.muni_cod">{{ municipio.muni_nomb }}</option>
+              <select class="form-control" id="muni_codi" v-model="comuna.muni_codi">
+                <option v-for="municipio in municipios" :value="municipio.muni_codi">
+                  {{ municipio.muni_nomb }}
+                </option>
               </select>
             </div>
           </div>
 
           <button class="btn btn-primary" type="submit">Actualizar</button>
-          <button class="btn btn-secondary mx-2" @click="$router.push({ name: 'Comunias' })">Cancelar</button>
+          <button class="btn btn-secondary mx-2" @click="$router.push({ name: 'Comunas' })">Cancelar</button>
         </form>
       </div>
     </div>
@@ -58,39 +61,32 @@ export default {
   name: 'EditarComuna',
   data() {
     return {
-      comuna: {
-        comu_cod: '',
-        comu_nomb: '',
-        muni_cod: ''
-      },
+      comuna: null, // Se inicializa como null para usar v-if
       municipios: []
     }
   },
   methods: {
-    cancelar() {
-      this.$router.push({ name: 'Comunas' })
-    },
     async updateComuna() {
-      const res = await axios.put(`http://127.0.0.1:8000/api/comunas/${this.comuna.comu_cod}`, this.comuna)
+      const res = await axios.put(`http://127.0.0.1:8000/api/comunas/${this.comuna.comu_codi}`, this.comuna);
       if (res.status === 200) {
-        this.$router.push({ name: 'Comunas' })
+        this.$router.push({ name: 'Comunas' });
         Swal.fire({
           position: 'top-end',
           icon: 'success',
-          title: 'Comuna has been updated',
+          title: 'Comuna actualizada correctamente',
           showConfirmButton: false,
           timer: 2000
-        })
+        });
       }
     }
   },
   mounted() {
-    this.comuna.comu_codi = this.$route.params.id
-    axios.get(`http://127.0.0.1:8000/api/comunas/${this.comuna.comu_cod}`)
+    const id = this.$route.params.id;
+    axios.get(`http://127.0.0.1:8000/api/comunas/${id}`)
       .then(response => {
-        this.comuna = response.data.comuna
-        this.municipios = response.data.municipios
-      })
+        this.comuna = response.data.comuna;
+        this.municipios = response.data.municipios;
+      });
   }
 }
 </script>
